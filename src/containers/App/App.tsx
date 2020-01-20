7e00533a1b1be5d59b6a4aea2d659a8adab8067f
il.ya sald.in <saldin.iliya@gmail.com>
2020-01-21 00:51:48 +0600

    Update apollo version

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 import MainPage from '@app/pages/main';
 
 import { PlayerContainerPortal } from '@app/containers/Player';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+  }),
 });
 
 class App extends React.Component<{}, {}> {
--- src/pages/main/topRatedArtistsQuery.graphql.ts
+++ src/pages/main/topRatedArtistsQuery.graphql.ts
@@ -1,4 +1,4 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
 
 export default gql`
   query TopRatedArtists($sort: TopRatedArtistsSort = JUDGES_RATING, $limit: Int = 25) {
