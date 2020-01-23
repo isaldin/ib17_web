4c732cfa2d562fd8ae4108ed659a457178312ea8
il.ya sald.in <saldin.iliya@gmail.com>
2020-01-23 22:23:39 +0600

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
   rules: {
     'consistent-return': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     'prettier/prettier': 'off',
   },
 };
